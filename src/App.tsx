e8c42455bc8de9e8e8532735c750bfc5304ab970
Olavo Licursi <55329614+olavo-licursi@users.noreply.github.com>
2021-02-10 16:26:03 -0300

    AuthContext e Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-
-// import SignIn from './pages/Signin';
+import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
+    <ToastContainer />
+
     <GlobalStyle />
   </>
 );
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,42 +1,88 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
+import * as Yup from 'yup';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/AuthContext';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
-      <form>
-        <h1>Faça seu login</h1>
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
+  const { signIn } = useAuth();
 
-        <Input
-          name="password"
-          type="password"
-          icon={FiLock}
-          placeholder="Senha"
-        />
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
 
-        <Button type="submit">Entrar</Button>
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
 
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+          formRef.current?.setErrors(errors);
+        }
+      }
+    },
+    [signIn],
+  );
 
-    <Background />
-  </Container>
-);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu login</h1>
+
+          <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            type="password"
+            icon={FiLock}
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
